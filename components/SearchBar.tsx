304beec0e35430e49f377c0771d5b9b9e704e012
ansh-pachauri <yepitsansh2@gmail.com>
2025-09-23 12:36:05 +0530

    first commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Image, TextInput } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+const SearchBar = () => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-fullpx-5 py-4'>
+      <Image source={icons.search} className='size-5'
+      resizeMode='contain' tintColor="#ab8bff" />
+      <TextInput
+      onPress={() => {}}
+      value=''
+      placeholder='Search'
+      onChangeText={() => {}}
+      placeholderTextColor="#a8b5db"
+      className='flex-1 ml-2 text-white'
+
+      />
+    </View>
+  )
+}
+
+export default SearchBar
